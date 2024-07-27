c44fba1907645b63e171e9cd6e91d4eaf53d5fd4
Chuong185 <jonelamp81@gmail.com>
2024-07-27 11:49:43 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger.con");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
